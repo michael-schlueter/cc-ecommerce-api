f2fe0e59d1598f7036e8b59536c170d0015ad5ff
Michael Schlueter <schluet0r4life@gmail.com>
2022-12-13 14:50:22 +0100

    basic app setup

---
+++ index.ts
@@ -0,0 +1,20 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+const logger = require("morgan");
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello world");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
